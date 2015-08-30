0ad8cae012dde335fc9b32ea85e6845be570dba3
gsytec <gsytec@gmail.com>
2015-08-30 15:12:24 +0530

    first commit-1

---
+++ SimpleMavenProject/src/main/java/pkg5/voda/org/SimpleMavenProject/App.java
@@ -0,0 +1,13 @@
+package pkg5.voda.org.SimpleMavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
